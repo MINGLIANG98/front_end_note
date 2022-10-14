abfb24ad89141859c9d437f03599f1fd3f88da34
qianmingliang <1278586129@qq.com>
2022-10-14 17:31:45 +0800

    update md

--- test.js
+++ test.js
@@ -16,3 +16,73 @@ array.map((e) => (typeof (e)));  // [12, 1, 3.1415, -10.01]
 const c = console.log.bind(document)
 
     instanceof ([])
+
+record.itemAmount = ((record.offerUnitPrice ? record.offerUnitPrice : 0) * (record.itemCount ? record.itemCount : 0)).toFixed(2); save(record)
+
+record.faultToleranceRateAbsolute = (record.faultToleranceRate / 100 * record.itemCount).toFixed(2); save(record)
+
+record.itemAmount = ((record.offerUnitPrice ? record.offerUnitPrice : 0) * (record.itemCount ? record.itemCount : 0)).toFixed(2); save(record)
+
+record.rest = (record.total - (record.yewu ?? 0) - (record.produceHost ?? 0) - (record.qualityControl ?? 0) - (record.outSource ?? 0) - (record.custom ?? 0)); save(record)
+
+
+
+
+record.rest = (record.total - (record.yewu ?? 0) - (record.produceHost ?? 0) - (record.qualityControl ?? 0) - (record.outSource ?? 0) - (record.custom ?? 0)); save(record)
+record.regularRatio = ((record.regular ?? 0) + (record.inregular ?? 0)); save(record)
+
+record.regularRatio = Number(record.regular) > 0 ? Number((record.regular ?? 0) / ((record.regular ?? 0) + (record.inregular ?? 0)).toFixed(2)) * 100 + '%' : '0%'; save(record);
+
+Number((1) / ((1) + (62))).toFixed(2)
+record.regularRatio = Number(record.regular) > 0 ? Number((record.regular ?? 0) / ((record.regular ?? 0) + (record.inregular ?? 0))).toFixed(2) * 100 + '%' : '0%'; save(record);
+<div>
+    {!!text ? (
+        text.map(file => (
+            <a href={file.url} download={file.name}>
+                {file.name}
+            </a>
+        ))
+    ) : (
+        ''
+    )}
+</div>
+
+record.regularRatio = Number(record.regular) > 0 ? Math.round((record.regular ?? 0) / ((record.regular ?? 0) + (record.inregular ?? 0)) * 10000) / 100.00 + "%" : '0%'; save(record);
+
+parseFloat((Number((11) / ((11) + (2))) * 100).toPrecision(12)) + '%'
+
+
+record.rest = (Math.floor(record.total ?? 0) - Math.floor(record.yewu ?? 0) - Math.floor(record.produceHost ?? 0) - Math.floor(record.qualityControl ?? 0) - Math.floor(record.outSource ?? 0) - Math.floor(record.custom ?? 0)); save(record)
+
+
+
+[
+    { required: true, message: '字段不能为空!' },
+    {
+        min: 0,
+        message: '字段不可小于零',
+    }
+]
+
+[
+    { "required": true, "message": '字段不能为空!' },
+    {
+        "min": 0,
+        type: 'number',
+        message: '字段不可小于零',
+    }
+    {
+        "_fun":
+            // ({ getFieldValue }) => ({
+    //     validator(_, value) {
+    //         const total = getFieldValue('total')
+    //         if (!!total && value > total) {
+    //             return Promise.reject('字段值不可大于总分数');
+    //         }
+    //         return Promise.resolve();
+    //     },
+    // })
+    }
+]
+
+[{"validator":"console.log('***',{record,value,rule,cc});","message":"校验错误"}]
