2d51eb8269be9a693ff53f336d638e802ff3d64d
QIANMINGLIANG <q1278586129@gmail.com>
2024-07-25 09:20:38 +0800

    worker

--- worker.js
+++ worker.js
@@ -9,3 +9,35 @@ onmessage = function (e) {
     postMessage(workerResult);
   }
 }
+
+// !! webworker线程中不能使用一些es6新特性 如可选链等
+/** https://www.cnblogs.com/xiaonian8/p/15149666.html
+ * import RequestWorker from '@/utils/request.worker.js?v=1'  //??可使用连接参数的方式 刷新编译缓存
+ *         rules: [
+          {
+            test: /\.worker\.js$/,
+            use: {
+              loader: 'worker-loader',
+              options: {
+                inline: true,
+                // loader 2 api使用方式
+                // name: '[name]:[hash:8].js', // 打包后的chunk的名称
+                // inline: true, // 开启内联模式，将chunk的内容转换为Blob对象内嵌到代码中
+              },
+            },
+          },
+        ],
+ * 
+ * 1.全局实例化：
+    适用于需要在整个应用中共享的长时间运行的 Worker。
+    实例化后可以复用，不必频繁创建和销毁。
+
+    2.组件内实例化：
+    适用于需要独立管理的页面或组件。
+    在组件的 mounted 钩子中创建，在 beforeDestroy 钩子中销毁。
+
+    3.事件触发实例化：
+    适用于短时间任务，任务完成后立即销毁 Worker。
+    避免频繁创建和销毁 Worker 以节省资源。
+ */
+
